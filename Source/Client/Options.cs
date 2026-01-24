5ab6cc92f5e8890d091fb829861d9799c3402a21
Justin Grote <justingrote@users.noreply.github.com>
2026-01-24 11:04:27 -0800

    🧹 Cleanup Options for new System.Commandline formatting.

--- Source/Client/Options.cs
+++ Source/Client/Options.cs
@@ -10,40 +10,40 @@ public static class Options
 {
 	public static readonly Option<string> Script = new("--script", "-c")
 	{
-		Description = "The PowerShell script to execute."
+		Description = "A PowerShell script string to execute. Example: \"'Hello World'\""
 	};
 
-	public static readonly Option<FileInfo> File = new("--file")
+	public static readonly Option<FileInfo> File = new("--file", "-f")
 	{
-		Description = "Path to a PowerShell script file to execute.",
+		Description = "Path to a PowerShell script file to execute",
 	};
 
 	public static readonly Option<DirectoryInfo> WorkingDirectory = new("--working-directory", "-w")
 	{
-		Description = "The working directory to use when spawning the PowerServe server if needed.",
+		Description = "The working directory to use when spawning the PowerServe server",
 		DefaultValueFactory = x => new DirectoryInfo(Environment.CurrentDirectory)
 	};
 
 	public static readonly Option<string> PipeName = new("--pipe-name", "-p")
 	{
-		Description = "The named pipe to use. The server will start here if not already running. Defaults to PowerServe-{username}.",
+		Description = "The named pipe to use. The server will start here if not already running",
 		DefaultValueFactory = x => $"PowerServe-{Environment.UserName}"
 	};
 
 	public static readonly Option<bool> Verbose = new("--verbose", "-v")
 	{
-		Description = "Log verbose messages about what PowerServeClient is doing to stderr. This may interfere with the JSON response so only use for troubleshooting."
+		Description = "Log verbose messages about what PowerServeClient is doing to stderr. This may interfere with the JSON response so only use for troubleshooting"
 	};
 
 	public static readonly Option<int> Depth = new("--depth", "-d")
 	{
-		Description = "The maximum depth for JSON serialization of PowerShell objects. Defaults to 2.",
+		Description = "The maximum depth for JSON serialization of PowerShell objects. Set to a higher value if you need more in-depth information but this may slow processing, it is better to keep your objects shallow if possible.",
 		DefaultValueFactory = x => 2
 	};
 
 	public static readonly Option<DirectoryInfo> ExeDir = new("--exeDir", "-e")
 	{
-		Description = "The directory to use to find pwsh.exe when spawning the PowerServe server if needed. Defaults to the directory of the current executable.",
+		Description = "The directory to use to find pwsh.exe when spawning the PowerServe server if needed. Defaults to the directory of the current executable",
 		DefaultValueFactory = x => new DirectoryInfo(Environment.CurrentDirectory)
 	};
 
